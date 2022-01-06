04011214ea6f1cadb022c38a01664e114700af2a
adriano25klein@gmail.com <you@example.com>
2022-01-06 09:12:36 -0300

    Configurei o Nodemon

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const config = require("config");
-const supplierRoutes = require("./routes/suppliers");
+const supplierRoutes = require("./api/routes/suppliers/index");
 
 app.use(express.json());
 
